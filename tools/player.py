2b86bfbeab85808c453b6af6fd43c5290ce7ae0a
Florent Barre <florent.barre.pro@gmail.com>
2022-03-12 16:12:43 +0100

    (tools/player.py) add id and nb_life

--- tools/player.py
+++ tools/player.py
@@ -1,6 +1,8 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, id, name, nb_life):
+        self.id = id
         self.name = name
+        self.nb_life = nb_life
         self.cards = []
 
     def add_card(self, card):
