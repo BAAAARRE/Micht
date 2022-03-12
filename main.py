1192cdfc9e5865fa1f8424fb37238f262acb7469
Florent Barre <florent.barre.pro@gmail.com>
2022-03-12 16:32:49 +0100

    (tools/micht.py) add heritance between round and game

--- main.py
+++ main.py
@@ -2,52 +2,50 @@ from tools import micht
 
 
 def main():
-    game = micht.Micht()
+    game = micht.Game()
+    game.register_player()
 
-    while True:
-        player_name = input("Ajouter le nom du joueur, appuyer 'Entrée' si vous avez finis : ")
-        if len(player_name) > 0:
-            game.register_player(player_name)
-            print(f"{player_name} a été ajouté")
-        else:
-            print("\nLe micht commence !")
-            break
-
-    game.update_nb_players()
-    game.distribute_cards(3)
-    print("Les cartes ont été distribué\n")
-
-    while not game.liar:
+    nb_games = 2
+    for num_game in range(nb_games):
+        print(num_game)
+        game.distribute_cards(3)
         for player in game.players:
-            player_cards = ", ".join([card.full_name for card in player.cards])
-            print(f"C'est à {player.name} de jouer")
-            ready_to_play = input("Tu es prêt à jouer? Appuis sur 'Entrée'")
+            print(player.name)
+            for card in player.cards:
+                print(card.full_name)
+        print("Les cartes ont été distribué\n")
 
-            if ready_to_play == "":
-                print(f"Tes cartes sont {player_cards}")
-
-                if len(game.current_call) == 0:
-                    print(f"\nC'est à toi de faire le premier call")
-                    game.current_call = input("Combinaison à call : ")
-                    game.last_player = player.name
-                    game.last_player_cards = player_cards
-                    print("\n")
-
-                elif len(game.current_call) > 0:
-                    print(f"\n{game.last_player} a call '{game.current_call}'")
-                    liar_input = input("Est ce que c'est un menteur (y/n)").lower()
-
-                    if liar_input == "y":
-                        print(f"{game.last_player} a call '{game.current_call}' et il avait {game.last_player_cards}")
-                        game.liar = True
-                        break
-
-                    elif liar_input == "n":
-                        print(f"\nC'est à toi de faire le call")
-                        game.current_call = input("Combinaison à call : ")
-                        game.last_player = player.name
-                        game.last_player_cards = player_cards
-                        print("\n")
+    # while not game.liar:
+    #     for player in game.players:
+    #         player_cards = ", ".join([card.full_name for card in player.cards])
+    #         print(f"C'est à {player.name} de jouer")
+    #         ready_to_play = input("Tu es prêt à jouer? Appuis sur 'Entrée'")
+    #
+    #         if ready_to_play == "":
+    #             print(f"Tes cartes sont {player_cards}")
+    #
+    #             if len(game.current_call) == 0:
+    #                 print(f"\nC'est à toi de faire le premier call")
+    #                 game.current_call = input("Combinaison à call : ")
+    #                 game.last_player = player.name
+    #                 game.last_player_cards = player_cards
+    #                 print("\n")
+    #
+    #             elif len(game.current_call) > 0:
+    #                 print(f"\n{game.last_player} a call '{game.current_call}'")
+    #                 liar_input = input("Est ce que c'est un menteur (y/n)").lower()
+    #
+    #                 if liar_input == "y":
+    #                     print(f"{game.last_player} a call '{game.current_call}' et il avait {game.last_player_cards}")
+    #                     game.liar = True
+    #                     break
+    #
+    #                 elif liar_input == "n":
+    #                     print(f"\nC'est à toi de faire le call")
+    #                     game.current_call = input("Combinaison à call : ")
+    #                     game.last_player = player.name
+    #                     game.last_player_cards = player_cards
+    #                     print("\n")
 
 
 if __name__ == "__main__":
--- tools/micht.py
+++ tools/micht.py
@@ -2,26 +2,8 @@ from tools.player import Player
 from tools.deck import Deck
 
 
-class Game:
+class Round:
     def __init__(self):
-        self.players = []
-        self.nb_players = int(input("Entrez le nombre de joueurs : "))
-        self.nb_life = int(input("Nombre de vie par joueurs : "))
-
-    def add_player(self, id, player_name):
-        player = Player(id, player_name)
-        self.players.append(player)
-
-    def register_player(self):
-        for num_player in range(self.nb_players):
-            id_player = num_player + 1
-            player_name = input(f"Ajouter le nom du joueur n°{id_player} : ")
-            self.add_player(id_player, player_name, self.nb_life)
-
-
-class Round(Game):
-    def __init__(self):
-        super().__init__()
         self.liar = False
         self.current_call = ""
         self.last_player = ""
@@ -32,7 +14,26 @@ class Round(Game):
         sub_deck = [full_cards[i:i+nb_cards_to_distribute] for i in range(0, len(full_cards), nb_cards_to_distribute)]
         num_sub_deck = 0
         for player in self.players:
+            player.reinitialize_cards()
             for card in sub_deck[num_sub_deck]:
                 player.add_card(card)
             num_sub_deck += 1
+
+
+class Game(Round):
+    def __init__(self):
+        super().__init__()
+        self.players = []
+        self.nb_players = int(input("Entrez le nombre de joueurs : "))
+        self.nb_life = int(input("Nombre de vie par joueurs : "))
+
+    def add_player(self, id, player_name, nb_life):
+        player = Player(id, player_name, nb_life)
+        self.players.append(player)
+
+    def register_player(self):
+        for num_player in range(self.nb_players):
+            id_player = num_player + 1
+            player_name = input(f"Ajouter le nom du joueur n°{id_player} : ")
+            self.add_player(id_player, player_name, self.nb_life)
 
